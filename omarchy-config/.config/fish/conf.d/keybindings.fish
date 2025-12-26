e32d6bd9d15131be78e064bb1a490ca0146f1949
Justin Carlson <40642470+justcarlson@users.noreply.github.com>
2025-12-26 16:45:21 -0500

    fix(fish): add Alt+Backspace and Ctrl+Backspace keybindings
    Bind backward-kill-word to Alt+Backspace and Ctrl+Backspace in insert mode.
    Fish shell with vi mode (Omarchy default) doesn't include these standard
    word-deletion keybindings by default.
    
    Fixes #111

---
+++ omarchy-config/.config/fish/conf.d/keybindings.fish
@@ -0,0 +1,5 @@
+# Word deletion keybindings
+# Fish doesn't bind these by default, unlike bash/zsh
+
+bind -M insert alt-backspace backward-kill-word
+bind -M insert ctrl-backspace backward-kill-word
